1a9b0d2e08cd8b0aafc2a4296c38cc808f2bf028
Skvortsov Sergey <majitel@list.ru>
2017-06-07 15:25:52 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('app-sass', function () {
+    gulp.src('app/scss/main.scss')
+       .pipe(sass())
+       .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+       .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', ['app-sass']);
+});
+
+gulp.task('build', ['app-sass'], function() {
+
+});
+
+gulp.task('default', ['watch', 'build']);
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
