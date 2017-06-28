c0cf5c612a49ce8b7e7e89e03cd44624326bf6ae
Skvortsov Sergey <majitel@list.ru>
2017-06-28 10:13:32 +0300

    4

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,14 +4,28 @@ $(document).ready(function () {
         infinite: true,
         arrows: true,
         dots: true,
-        rows: 3,
-        slidesPerRow: 9,
+        rows: 4,
+        slidesPerRow: 6,
         responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    rows: 3,
+                    slidesPerRow: 5
+                }
+            },
             {
                 breakpoint: 960,
                 settings: {
-                    rows: 4,
+                    rows: 3,
                     slidesPerRow: 4
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    rows: 3,
+                    slidesPerRow: 3
                 }
             }
         ]
